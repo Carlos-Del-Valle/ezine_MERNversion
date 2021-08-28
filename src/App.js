c18234847415fad834dcc40aac1cec3df0eda41b
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-08-28 17:46:35 +1000

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Home from './pages/Home';
 import {BrowserRouter as Router, Route} from "react-router-dom";
+
+//Components
+import Home from './pages/Home';
+import Article from './pages/Article';
+import About from './pages/About';
+import ArticlesList from './pages/ArticlesList';
 
 function App() {
   return (
       <Router>
     <div className='max-w-screen-md mx-auto pt-20'>
       <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/articles-list" component={ArticlesList}/>
+      <Route exact path="/article" component={Article}/>
     </div>
       </Router>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div className="mb-20">
+            <h1 className="sm-text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">About FILLOS DUN CAN</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">BENVIDOS, FILLOS DUN CAN, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam autem beatae commodi consequatur debitis, delectus dignissimos dolorem doloremque dolores enim et eveniet facere harum magnam minima minus modi nostrum nulla obcaecati officia officiis optio perspiciatis, quod rem voluptas. Ad animi consequatur deserunt doloribus ea laborum magnam maiores minus natus nostrum numquam perferendis perspiciatis, quaerat sequi suscipit, vel voluptates.
+                Amet asperiores aspernatur beatae cum distinctio explicabo, magnam molestias necessitatibus, possimus quae quas quisquam repellat vel? Blanditiis consequatur dolores est excepturi explicabo, minima porro quo rem sed sit sunt tenetur velit! Dolore iure nam numquam. Enim facilis laudantium quam quia sunt. Repudiandae?</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam atque dignissimos ipsum labore, magni, modi praesentium quasi recusandae reiciendis sint voluptas!
+                Architecto consequuntur dolorem eligendi eum, ipsa libero odit possimus quod suscipit veniam? At autem eveniet illum itaque iusto magnam officiis porro praesentium, quisquam quod repellendus repudiandae, sequi, veritatis.</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto nihil placeat? Delectus doloremque facilis ipsa! Iure, omnis, quisquam. Impedit.</p>
+        </div>
+    )
+}
+
+export default About
