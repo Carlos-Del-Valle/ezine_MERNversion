719fd8c513e9916b8ce3c9bf1df3867ddafac5b4
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-08-30 18:28:47 +1000

    Add React Router

---
+++ src/pages/article-content.js
@@ -0,0 +1,20 @@
+const articles = [
+    {
+
+        name: "learn-react", //Ojo aqu'i que esto ser'a la URL
+        title: "A VER O CO REACT",
+        content: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum commodo ipsum, id sodales magna pellentesque sed. Duis tincidunt sem eu pulvinar vulputate. Phasellus sed nunc eu risus volutpat varius. Nam at neque eget justo efficitur semper id vitae est. Sed nec massa eu ipsum tempus commodo at sit amet magna. Nunc non metus pulvinar, rhoncus magna eget, hendrerit eros. Proin cursus imperdiet consectetur. Sed gravida ligula nec orci aliquet, in finibus quam pharetra. Aliquam fermentum eleifend tincidunt. Morbi suscipit, risus sit amet accumsan ultrices, tortor arcu dignissim felis, eu molestie lorem justo in erat. Nam consequat malesuada metus ac rutrum. Sed suscipit tellus ac nulla sodales malesuada. Etiam rutrum scelerisque mauris, non lacinia massa cursus vitae. In suscipit arcu est, eget molestie nulla porta id. Aenean eu tempus sem. Donec quis maximus ligula.\n" +
+            "\n" +
+            "Etiam aliquet nisl ut tempus auctor. Sed sed fringilla sem. Nam eu sapien vestibulum, congue eros eget, scelerisque felis. Maecenas fermentum neque et egestas congue. Vivamus sagittis lorem erat, eget blandit est blandit eu. Mauris aliquam in tortor quis bibendum. Cras id justo dignissim leo pharetra efficitur. Duis aliquet tincidunt lacinia. Nullam ut finibus odio, sed pretium risus. Maecenas commodo gravida magna, consectetur semper quam dignissim dignissim.\n" +
+            "\n" +
+            "Ut in ultrices nibh, nec finibus lacus. Fusce blandit risus turpis, sit amet egestas neque ullamcorper id. Nam bibendum finibus aliquet. Suspendisse sed nibh pellentesque nisl sollicitudin ullamcorper. Nunc dignissim metus vel dui facilisis, malesuada vestibulum velit suscipit. Proin non elit est. Nunc pulvinar pharetra tincidunt. In et lorem id augue mollis mollis. Duis hendrerit, massa sed ultricies posuere, arcu ligula consequat sem, sit amet dapibus nisi odio non odio. Suspendisse in imperdiet erat.\n" +
+            "\n" +
+            "Ut lobortis nibh nec ligula pulvinar finibus. In eu diam lacinia, convallis metus et, accumsan ligula. Vivamus et vestibulum sem. Nam scelerisque metus et commodo dapibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In quis nisl imperdiet, iaculis quam ac, viverra lectus. Proin mauris purus, pulvinar sit amet egestas id, interdum sed mi. Proin eu lorem dapibus, bibendum enim non, commodo nulla. Etiam eleifend augue at elit dapibus pellentesque. Etiam feugiat elit sit amet diam condimentum malesuada. Donec et imperdiet magna. Nulla aliquam sagittis orci id auctor. Nunc eu elit finibus, pharetra diam at, tristique ex. Nunc et lectus quis dui molestie congue. Quisque vel bibendum arcu. Suspendisse sollicitudin, arcu et volutpat blandit, felis urna iaculis neque, tristique feugiat arcu enim commodo libero.\n" +
+            "\n" +
+            "Morbi mauris nisl, commodo vitae ultricies eget, porta ac augue. Integer non velit ut lectus tincidunt consequat. Sed ac justo ac purus dapibus fringilla. Nunc in nulla vitae sapien molestie vulputate non sed turpis. Donec tincidunt aliquet purus, aliquet pretium mi convallis nec. Etiam non nisl ullamcorper, gravida massa et, blandit turpis. Mauris magna nisl, vulputate in libero suscipit, facilisis feugiat massa. Proin vulputate mauris sit amet orci varius mattis ac vel ex. Nulla facilisi. Proin vulputate nisi risus, id tempus lectus consectetur nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin imperdiet vel dolor sit amet rhoncus."
+        ]
+    }
+]
+
+export default articles;
