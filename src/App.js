256c24c4f106007c7ebc4836f85fe8bd3da86245
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-08-30 22:18:41 +1000

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 //Pages
 import Home from './pages/Home';
 import Article from './pages/Article';
 import About from './pages/About';
 import ArticlesList from './pages/ArticlesList';
+import NotFound from "./pages/NotFound";
 
 //Components
 import NavBar from "./components/NavBar";
@@ -14,11 +15,14 @@ function App() {
       <Router>
         <NavBar />
     <div className='max-w-screen-md mx-auto pt-20'>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/about" component={About}/>
-      <Route exact path="/articles-list" component={ArticlesList}/>
-      <Route exact path="/article/:name" component={Article}/>
-    </div>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/articles-list" component={ArticlesList}/>
+        <Route exact path="/article/:name" component={Article}/>
+        <Route component={NotFound} />
+      </Switch>
+      </div>
       </Router>
   );
 }
